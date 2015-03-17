e0ca40ec736eed1f717e03d9e772c8f49b9dbd15
Andrew Later <a_later86@hotmail.com>
2015-03-17 11:45:05 -0600

    main buttons

--- src/StoreLayoutPanel.java
+++ src/StoreLayoutPanel.java
@@ -10,23 +10,39 @@ import javax.swing.*;
 public class StoreLayoutPanel extends JPanel{
 	private JButton addDepartment = new JButton("Add a Department");
 	private JTextField search = new JTextField("Enter Department Name:", 20);
+	private JButton deli = new JButton("Deli");
+	private JButton produce = new JButton("Produce");
+	private JButton bakery = new JButton("Bakery");
+	private JButton cleaning = new JButton("Cleaning");
+	private JButton frozenFood = new JButton("Frozen Food");
+	private JButton dryGoods = new JButton("Dry Foods");
+	private JButton seasonal = new JButton("Seasonal");
+	private JButton meat = new JButton("Meat");
+	
 	public boolean show = false;
 	public StoreLayoutPanel(){
 		ButtonListener listener = new ButtonListener();
 		addDepartment.addActionListener(listener);
 		search.addActionListener(listener);
 		
-		this.add(addDepartment);
-		this.add(search);
+		this.add(deli);
+		this.add(produce);
+		this.add(bakery);
+		this.add(meat);
+		this.add(cleaning);
+		this.add(frozenFood);
+		this.add(dryGoods);
+		this.add(seasonal);
+		
 		this.setBackground(Color.gray);
-		this.setPreferredSize(new Dimension(600, 300));
+		this.setPreferredSize(new Dimension(300, 200));
 	}
 
 	public class ButtonListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource()== addDepartment){
-				search.setText("Enter Department Name");
+			if(e.getSource()== deli){
+				new Department("Deli");
 			}
 			else if(e.getSource()==search){
 				new Department(search.getText());
